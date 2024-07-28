5279b8bd4a7f13807b9600eaefd2b65726362d1e
giorgio12345678 <99029527+giorgio12345678@users.noreply.github.com>
2024-07-28 12:17:26 +0800

    --update convert to php and add backend&mysql

---
+++ common/footer.php
@@ -0,0 +1,4 @@
+<footer>
+  <span>Created By <a href="https://www.larylineage.com">Li-yan Huang</a> | <span class="far fa-copyright"></span>
+    2024 All rights reserved.</span>
+</footer>
--- html/index.php
+++ html/index.php
@@ -1,44 +1,71 @@
+<?php
+$conn = new mysqli("localhost", "root", "root", "lyHuang_resume");
+if ($error = $conn->connect_error) {
+    die('資料庫連接失敗:' . $error);
+}
+if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+    $accessed = true;
+    // 進行端驗證
+
+    foreach (["name", "email", "subject", "message"] as $key => $value) {
+        if (empty($_POST[$value])) {
+            $accessed = false;
+            echo '<script> alert("' . $value . '不能為空!!") </script>';
+            break;
+        }
+    }
+    // 驗證通過
+    if ($accessed) {
+
+        $sql = "INSERT INTO message (name,email,subject,message) VALUES(?,?,?,?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param('ssss', $name, $email, $subject, $message);
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $subject = $_POST["subject"];
+        $message = $_POST["message"];
+        $stmt->execute();
+        $stmt->close();
+    }
+}
+$sql = "SELECT * FROM message ORDER BY id DESC limit 3";
+$result = $conn->query($sql);
+$data = $result->fetch_all(MYSQLI_ASSOC);
+$conn->close();
+
+?>
+
 <!DOCTYPE html>
 <!-- Created By CodingNepal -->
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Personal Portfolio Website</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-    <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+    <?php include "../common/head.php" ?>
+    <style>
+        .testlist {
+            background-color: #555;
+            padding: 20px 20px 60px 20px;
+        }
 
+        .testlist .lists {
+            background-color: #fff;
+            padding: 15px;
+            margin: 10px 0;
+            display: flex;
+            justify-content: flex-start;
+        }
+
+        .testlist .lists>div {
+            margin-right: 10px;
+        }
+    </style>
 </head>
 
 <body>
     <div class="scroll-up-btn">
         <i class="fas fa-angle-up"></i>
     </div>
-    <nav class="navbar">
-        <div class="max-width">
-            <div class="logo"><a href="#">Lo<span>go.</span></a></div>
-            <ul class="menu">
-                <li><a href="#home" class="menu-btn">Home</a></li>
-                <li><a href="#about" class="menu-btn">About</a></li>
-                <li><a href="#services" class="menu-btn">Services</a></li>
-                <li><a href="#skills" class="menu-btn">Skills</a></li>
-                <li><a href="#teams" class="menu-btn">Portfolio</a></li>
-                <li><a href="#contact" class="menu-btn">Contact</a></li>
-            </ul>
-            <div class="menu-btn">
-                <i class="fas fa-bars"></i>
-            </div>
-        </div>
-    </nav>
-
-
+    <?php include "../common/header.php" ?>
 
     <!-- home section start -->
     <section class="home" id="home">
@@ -264,35 +291,47 @@
                 </div>
                 <div class="column right">
                     <div class="text">Message me</div>
-                    <form action="#">
+
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                         <div class="fields">
                             <div class="field name">
-                                <input type="text" placeholder="Name" required>
+                                <input name="name" type="text" placeholder="Name" required>
                             </div>
                             <div class="field email">
-                                <input type="email" placeholder="Email" required>
+                                <input name="email" type="email" placeholder="Email" required>
                             </div>
                         </div>
                         <div class="field">
-                            <input type="text" placeholder="Subject" required>
+                            <input name="subject" type="text" placeholder="Subject" required>
                         </div>
                         <div class="field textarea">
-                            <textarea cols="30" rows="10" placeholder="Message.." required></textarea>
+                            <textarea name="message" cols="30" rows="10" placeholder="Message.." required></textarea>
                         </div>
                         <div class="button-area">
-                            <button type="submit">Send message</button>
+                            <button name="submitted" type="submit">Send message</button>
                         </div>
                     </form>
+                    <div class="testlist">
+                        <?php
+                        foreach ($data as $key => $value) {
+                            echo '
+                            <div class="lists">
+                                <div>姓名: ' . $value["name"] . '</div>
+                                <div>信箱: ' . $value["email"] . '</div>
+                                <div>主旨: ' . $value["subject"] . '</div>
+                                <div>內容: ' . $value["message"] . '</div>
+                            </div>
+                            ';
+                        }
+                        ?>
+                    </div>
                 </div>
             </div>
         </div>
     </section>
 
     <!-- footer section start -->
-    <footer>
-        <span>Created By <a href="https://www.larylineage.com">Li-yan Huang</a> | <span class="far fa-copyright"></span>
-            2024 All rights reserved.</span>
-    </footer>
+    <?php include "../common/footer.php" ?>
 
     <script src="script.js"></script>
 </body>
