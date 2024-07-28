73c64cbd8408741400e7998fae93e7dbe96b2d93
giorgio12345678 <99029527+giorgio12345678@users.noreply.github.com>
2024-07-28 17:31:55 +0800

    --update content

--- common/header.php
+++ common/header.php
@@ -1,13 +1,13 @@
 <nav class="navbar">
   <div class="max-width">
-    <div class="logo"><a href="#">Lo<span>go.</span></a></div>
+    <div class="logo"><a href="#">LI<span>YAN.</span></a></div>
     <ul class="menu">
-      <li><a href="#home" class="menu-btn">Home</a></li>
-      <li><a href="#about" class="menu-btn">About</a></li>
-      <li><a href="#services" class="menu-btn">Services</a></li>
-      <li><a href="#skills" class="menu-btn">Skills</a></li>
-      <li><a href="#teams" class="menu-btn">Portfolio</a></li>
-      <li><a href="#contact" class="menu-btn">Contact</a></li>
+      <li><a href="#home" class="menu-btn">首頁</a></li>
+      <li><a href="#about" class="menu-btn">關於我</a></li>
+      <li><a href="#services" class="menu-btn">工作內容</a></li>
+      <li><a href="#skills" class="menu-btn">技能</a></li>
+      <li><a href="#teams" class="menu-btn">作品集</a></li>
+      <li><a href="#contact" class="menu-btn">聯絡我</a></li>
     </ul>
     <div class="menu-btn">
       <i class="fas fa-bars"></i>
